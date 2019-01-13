21c8c30570d38ca3a8c7e898e62f99fc9c8d1eb0
onlysw <45906997+onlysw@users.noreply.github.com>
2019-01-13 10:29:21 +0530

    plot different type of graph

---
+++ plot2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({
+    'name':['john','mary','peter','jeff','bill','lisa','jose'],
+    'age':[23,78,22,19,45,33,20],
+    'gender':['M','F','M','M','M','F','M'],
+    'state':['california','dc','california','dc','california','texas','texas'],
+    'num_children':[2,0,0,3,2,1,4],
+    'num_pets':[5,1,0,5,2,2,3]
+})
+print(df)
+df.plot(kind='scatter',x='num_children',y='num_pets',color='red')
+plt.show()
+df.plot(kind='bar',x='name',y='age')
+plt.show()
+
+
+ax = plt.gca()
+
+df.plot(kind='line',x='name',y='num_children',ax=ax)
+df.plot(kind='line',x='name',y='num_pets', color='red', ax=ax)
+
+plt.show()
